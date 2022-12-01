5c101d8ca66d5739ecf6f422b04d01825c430ed0
Yeko101 <117126685+Yeko101@users.noreply.github.com>
2022-12-01 00:13:38 +0000

    Add files via upload

---
+++ Peer- graded assignment.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# Dedume Yeko Kofi
+# Data scientist/Analyst
+
+# *I am interested in data science because I have passion for programming and writing algorithms to solve problems and queries.*
+
+# The code takes two numbers and gives you the product of the two numbers divided by their sum
+
+# In[5]:
+
+
+print("***welcome***")
+a = int(input("Enter first number: "))
+
+b= int(input("Enter second number: "))
+
+print((a*b)/(a+b))
+
+
+# * water
+# * fire
+# * earth
+# 1. mercury
+# 2. venus
+# 3. earth
+# 
+# ~~ama~~
+# 
+
+# In[ ]:
+
+
+
+
